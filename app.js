2c5f8c0924dcd28eb27fabc85ecc8ff97f20d8aa
Lavanya Rao <lrao@paciolan.com>
2018-08-28 19:46:37 -0700

    commit message

--- app.js
+++ app.js
@@ -80,6 +80,7 @@ app.use(function(req, res, next){
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
   res.locals.user = req.user || null
+  res.locals.adminUser = (req.user && req.user.accountType === 'ADMIN') || null
   //console.log(req.user)
   next();
 });
