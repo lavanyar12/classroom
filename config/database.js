e69b147fad66a7da2fe7f4f162c381c8ad40a928
Lavanya Rao <>
2020-10-26 11:59:15 -0700

    remove logs

--- config/database.js
+++ config/database.js
@@ -7,6 +7,5 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging')
   module.exports = 
   { 
     mongoURI: 'mongodb://localhost/classroom',
-    //mongoURI: 'mongodb+srv://classroom:science101@cluster0.egfbt.mongodb.net/classDB'
   }
 }
